fa9058827c1a7cba96fc32379c62cac266887b63
BodeMurairi2 <b.murairi@alustudent.com>
2025-06-19 13:12:16 +0200

    clean repo

--- resume_website/js/send_email.js
+++ resume_website/js/send_email.js
@@ -1,7 +1,10 @@
-function sendEmail() {
-    const name = document.getElementById('name');
-    const email = document.getElementById('email');
-    const phone = document.getElementById('phone');
-    const subject = document.getElementById('subject');
-    window.alert('Email sent successfully to Bode Murairi');
-}
+(function () {
+      emailjs.init("IXSFGlXdT2ZnWreAw");
+    })();
+
+function sendEmail(form) {
+    emailjs.sendForm("bode-email200", "template_wze8jzk", form)
+      .then(() => alert("Message sent successfully!"))
+      .catch(error => alert("Sending failed: " + JSON.stringify(error)));
+    return false;
+  }
