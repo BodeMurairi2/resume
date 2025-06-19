4aa5b1ff4eb5d0f8d193c59c878c31f07f3866ca
BodeMurairi2 <b.murairi@alustudent.com>
2025-06-19 23:56:35 +0200

    Finale Submission

--- js/send_email.js
+++ js/send_email.js
@@ -1,10 +1,25 @@
+// js/contact.js
+
 (function () {
-      emailjs.init("IXSFGlXdT2ZnWreAw");
-    })();
+  emailjs.init("IXSFGlXdT2ZnWreAw");
+})();
+
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
 
 function sendEmail(form) {
-    emailjs.sendForm("bode-email200", "template_wze8jzk", form)
-      .then(() => alert("Message sent successfully!"))
-      .catch(error => alert("Sending failed: " + JSON.stringify(error)));
+  const email = form.email.value;
+
+  if (!isValidEmail(email)) {
+    alert("Please enter a valid email address.");
     return false;
   }
+
+  emailjs.sendForm("bode-email200", "template_wze8jzk", form)
+    .then(() => alert("Message sent successfully!"))
+    .catch(error => alert("Sending failed: " + JSON.stringify(error)));
+
+  return false;
+}
